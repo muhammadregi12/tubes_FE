9af1196857eec45a86944bcb6aff4d4f5fa752d5
muhammadregi12 <127029465+muhammadregi12@users.noreply.github.com>
2025-06-27 00:09:48 +0700

    cr

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -28,9 +28,9 @@
         <span class="text-xs mt-1">Arisan Group</span>
       </router-link>
 
-      <router-link to="/laporan" :class="[
+      <router-link to="/join" :class="[
         'flex flex-col items-center',
-        $route.path === '/laporan'
+        $route.path === '/join'
           ? 'text-blue-600 is-active'
           : 'text-gray-500',
       ]">
@@ -39,7 +39,7 @@
             d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm2 10a1 1 0 10-2 0v3a1 1 0 102 0v-3zm2-3a1 1 0 011 1v5a1 1 0 11-2 0v-5a1 1 0 011-1zm4-1a1 1 0 10-2 0v7a1 1 0 102 0V8z"
             clip-rule="evenodd" />
         </svg>
-        <span class="text-xs mt-1">Laporan</span>
+        <span class="text-xs mt-1">Join Arisan</span>
       </router-link>
     </div>
   </footer>
