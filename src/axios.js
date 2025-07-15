d345c253e8804ff29d549fb70ca7fdd775983190
muhammadregi12 <127029465+muhammadregi12@users.noreply.github.com>
2025-07-15 19:01:12 +0700

    hosting

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api' // sesuaikan dengan base API kamu
+  baseURL: 'http://localhost:8000/api'
 })
 
 api.interceptors.request.use(config => {
